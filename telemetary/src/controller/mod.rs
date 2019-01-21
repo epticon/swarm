dc0e56223cb502d7f7e02d95e31119017f512842
Tochukwu Nkemdilim <nkemdilimtochukwu@gmail.com>
2019-01-21 23:58:13 +0100

    Style(Spacing): Adopt 4 Spaces On ALl Files

--- telemetary/src/controller/mod.rs
+++ telemetary/src/controller/mod.rs
@@ -2,5 +2,5 @@ use crate::router::RequestJson;
 use actix_web::Error;
 
 pub(crate) fn get_index(req: &RequestJson) -> Result<String, Error> {
-  Ok(String::new())
+    Ok(String::new())
 }
--- telemetary/src/router/mod.rs
+++ telemetary/src/router/mod.rs
@@ -9,30 +9,30 @@ pub(crate) use self::includes::*;
 type Callback<T> = fn(&RequestJson) -> Result<T, RouterError>;
 
 pub(crate) struct Router<T: Serialize> {
-	inner: HashMap<String, Callback<T>>,
+    inner: HashMap<String, Callback<T>>,
 }
 
 impl<T: Serialize> Default for Router<T> {
-	fn default() -> Self {
-		Router {
-			inner: HashMap::new(),
-		}
-	}
+    fn default() -> Self {
+        Router {
+            inner: HashMap::new(),
+        }
+    }
 }
 
 impl<T> Router<T>
 where
-	T: Serialize,
+    T: Serialize,
 {
-	pub(crate) fn match_route(&self, path: &str) -> Callback<T> {
-		match self.inner.get(path) {
-			Some(cb) => *cb,
-			None => |_| Err(RouterError::InvalidRoute),
-		}
-	}
+    pub(crate) fn match_route(&self, path: &str) -> Callback<T> {
+        match self.inner.get(path) {
+            Some(cb) => *cb,
+            None => |_| Err(RouterError::InvalidRoute),
+        }
+    }
 
-	fn add_route(&mut self, path: &str, callback: Callback<T>) -> &Self {
-		self.inner.insert(path.to_string(), callback);
-		self
-	}
+    fn add_route(&mut self, path: &str, callback: Callback<T>) -> &Self {
+        self.inner.insert(path.to_string(), callback);
+        self
+    }
 }
